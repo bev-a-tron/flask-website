435c1e737e0136381b10ab46d8eb6b2ad6800f5c
bev-a-tron <beverly.a.lau@gmail.com>
2014-11-17 09:37:16 -0800

    [BL] Delete unnecessary lines

--- application.py
+++ application.py
@@ -2,9 +2,6 @@ from flask import Flask,render_template,request
 
 app = Flask(__name__)
 
-app.progvars={}
-app.num_clips=5
-
 @app.route('/')
 @app.route('/index',methods=['GET','POST'])
 def index():
